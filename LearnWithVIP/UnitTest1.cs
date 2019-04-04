875e3cd74f2c2cc1d72f94dcb11a714a85a58b1a
Vipin Kumar Yadav <er.vipinyadav@gmail.com>
2019-04-04 16:15:23 +0530

    Initial Empty Project

---
+++ LearnWithVIP/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace LearnWithVIP
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Debug.WriteLine("Hello");
+        }
+    }
+}
