8dfbe8cfa9b6580536df2faabd4261f91955fd98
Vipin Kumar Yadav <er.vipinyadav@gmail.com>
2019-07-15 15:28:04 +0530

    Remove Duplicate characters from String.

---
+++ CodingProblemsTests/StringProblemsTests/DuplicateCharactersInStringTests.cs
@@ -0,0 +1,24 @@
+﻿using LearnWithVIP.CodingProblems.StringProblems;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CodingProblemsTests.StringProblemsTests
+{
+    public class DuplicateCharactersInStringTests
+    {
+        [Fact]
+        public void TestDuplicateCharactersInString()
+        {
+            var input = "vipinyadav";
+            var output1 = DuplicateCharactersInString.RemoveDuplicateChars(input);
+            var output2 = DuplicateCharactersInString.RemoveDuplicateCharsByLinq(input);
+            var output3 = DuplicateCharactersInString.RemoveDuplicateCharsByHashset(input);
+
+            Assert.Equal("vipnyad", output1);
+            Assert.Equal("vipnyad", output2);
+            Assert.Equal("vipnyad", output3);
+        }
+    }
+}
---
+++ CompanyCodingProblems/StringProblems/DuplicateCharactersInString.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearnWithVIP.CodingProblems.StringProblems
+{
+    // How to remove duplicate characters from string.
+    /*
+                Input: vipinyadav
+                Output: vipnyad
+    */
+
+    public class DuplicateCharactersInString
+    {
+
+        public static string RemoveDuplicateChars(string str)
+        {
+            // --- Removes duplicate chars using string concats. ---
+            // Store encountered letters in this string.
+            string table = "";
+
+            // Store the result in this string.
+            string result = "";
+
+            // Loop over each character.
+            foreach (char ch in str)
+            {
+                // See if character is in the table.
+                if (table.IndexOf(ch) == -1)
+                {
+                    // Append to the table and the result.
+                    table += ch;
+                    result += ch;
+                }
+            }
+            return result;
+        }
+
+        public static string RemoveDuplicateCharsByLinq(string str)
+        {
+            return new string(str.ToCharArray().Distinct().ToArray());
+        }
+
+        // Use HashSet to map the string to char. 
+        // This will remove the duplicate characters from a string.
+        public static string RemoveDuplicateCharsByHashset(string str)
+        {
+            var unique = new HashSet<char>(str);
+            return new string(unique.ToArray());
+        }
+    }
+}
