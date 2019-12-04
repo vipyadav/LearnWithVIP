23d328d447a609b094fc87a5e8c7305be3d860a9
Vipin Kumar Yadav <er.vipinyadav@gmail.com>
2019-12-04 17:50:30 +0530

    Foreground Vs Background Thread in C# with example.

---
+++ Threading/ForegroundVsBackgroundThread/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace ForegroundVsBackgroundThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForegroundThread();
+            //BackgroundThread();
+            Console.WriteLine("Main Thread Quits..!");
+        }
+
+        static void ForegroundThread()
+        {
+            Thread objThread = new Thread(WorkerThread);
+            objThread.Name = "Foreground Thread";
+            objThread.Start();
+        }
+
+        static void BackgroundThread()
+        {
+            Thread background = new Thread(WorkerThread);
+            background.Name = "Background Thread";
+            background.IsBackground = true;
+            background.Start();           
+        }
+
+        private static void WorkerThread()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("{0} is Running......", Thread.CurrentThread.Name);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("{0} Quits..!", Thread.CurrentThread.Name);
+        }
+    }
+}
